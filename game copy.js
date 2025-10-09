f833335e9e2e2b1c41dd55e647f63c422d175828
aluhutmannn-ops <Aulhutmannn@gmail.com.au>
2025-10-10 00:43:53 +1100

    keyboard line around width 80%

--- game copy.js
+++ game copy.js
@@ -996,7 +996,7 @@
       position: "fixed",
       bottom: "0",
       left: "0",
-      width: "100%",
+      width: "80%",
       display: "none",
       flexDirection: "column",
       alignItems: "center",
